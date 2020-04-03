9d1ea1b575ac4a9ee9287a9e00715e89bc0b8a35
lvminga@yutong.com <441403517@qq.com>
2020-04-03 17:46:22 +0800

    ssr支持

--- wecircleServer/routes/index.js
+++ wecircleServer/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const Vue = require('vue')
+const renderer = require('vue-server-renderer').createRenderer({
+	template: require('fs').readFileSync('./views/index.template.html', 'utf-8')
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/ssr', function(req, res, next) {
+  const app = new Vue({
+    data: {
+      url: req.url
+    },
+    template: `<div>访问的 URL 是： {{ url }}</div>`
+  })
+
+
+  renderer.renderToString(app, (err, html) => {
+	console.log(html) // html 将是注入应用程序内容的完整页面
+	if (err) {
+      res.status(500).end('Internal Server Error')
+      return
+    }
+    res.end(html)
+  })
+
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
